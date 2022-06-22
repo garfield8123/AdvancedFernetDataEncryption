a13c74c48db43c82b4069fa79051412cb4949513
garfield8123 <34069094+garfield8123@users.noreply.github.com>
2022-06-21 22:13:50 -0700

    Upload a test file for the pip project

--- test/test_encryption.py
+++ test/test_encryption.py
@@ -9,6 +9,24 @@ def writeJson():
 # Reads all information from Token.json file and stores in the dictionary data
 def useJson():
     with open("Token.json") as Token:
-        data = json.load(Token)
-    return data
+        TokenJson = json.load(Token)
+    return TokenJson
 
+# Encrypts any plain text and uses the token that is stored to generate an encrypted text
+def encryption():
+    TokenJson = useJson()
+    plainText = input("Plan text: ")
+    print("Encrypted Text: ", dataEncrpytion(plainText, TokenJson.get("GenerateToken")))
+
+# Encrypts any encrypted text and generates the plain text with the token stored
+def decryption():
+    TokenJson = useJson()
+    encryptedText = input("Encrypted Text: ")
+    print("Decrypted Text: ", dataDecryption(encryptedText, TokenJson.get("GenerateToken")))
+
+# Generates a unique sessionToken and Key for user sessions (For Web Servers)
+def WebSession():
+    UsernameToken = input("User Session: ")
+    SessionToken, SessionKey = generateSessionToken(UsernameToken)
+    print ("Session Token: ", SessionToken)
+    print("Session Key: ", SessionKey)
