88002e235b285287b78a833516a46edb459dbba3
Garfield Yang <34069094+garfield8123@users.noreply.github.com>
2024-06-06 15:28:11 -0700

    Fixed spelling and utcfromtimestamp depreciated problem

--- AdvancedFernetDataEncryption.py
+++ AdvancedFernetDataEncryption.py
@@ -19,7 +19,7 @@ def generateSessionToken(username, MinLength=100, MaxLength=120):
     sessionToken = ''.join(random.choice(string.ascii_lowercase + string.digits + string.ascii_uppercase) for _ in range(random.randint(MinLength,MaxLength)))
     return encryption(username + ":" + sessionToken)
 
-def dataEncrpytion(text, MinLength=100, MaxLength=120):
+def dataEncryption(text, MinLength=100, MaxLength=120):
     #---- Generate a random 128 character password with password to show on the servers and files to save ----
     RandomText = ''.join(random.choice(text + string.ascii_lowercase + string.digits + string.ascii_uppercase + string.punctuation) for _ in range(random.randint(MinLength,MaxLength)))
     #---- Creates a random number within the bounds of the length of passwords (basically shoves text in a random location) ----
@@ -29,7 +29,7 @@ def dataEncrpytion(text, MinLength=100, MaxLength=120):
     #---- Combine all the random points and text together to store this password ----
     text = text[:TextPoint] + RandomText[:RandomTextPoint] + RandomToken + RandomText[RandomTextPoint:] + text[TextPoint:]
     TextToken, TextKey = encryption(text)
-    timestamp = datetime.utcfromtimestamp(Fernet(str.encode(RandomToken.split(":")[0])).extract_timestamp(str.encode(RandomToken.split(":")[1]))).strftime(''.join(random.choice(['%d','%H','%d','%M','%d','%S']) for _ in range(int(MinLength/4),int(MaxLength/2))))
+    timestamp = datetime.fromtimestamp(Fernet(str.encode(RandomToken.split(":")[0])).extract_timestamp(str.encode(RandomToken.split(":")[1]))).strftime(''.join(random.choice(['%d','%H','%d','%M','%d','%S']) for _ in range(int(MinLength/4),int(MaxLength/2))))
     RandomTextToken, RandomTextKey = encryption(RandomText)
     return TextKey.decode("utf-8") +":" + timestamp[0:random.randint(1, len(timestamp))] +"/" + TextToken.decode("utf-8") + ":" + timestamp[0:random.randint(1, len(timestamp))] +"/" + RandomTextToken.decode("UTF-8") + ":" + timestamp[0:random.randint(1, len(timestamp))] + "/" + RandomTextKey.decode("UTF-8") +":" + timestamp[0:random.randint(1, len(timestamp))]  + "/" + RandomToken
 
@@ -48,7 +48,7 @@ def dataDecryption(EncryptedText):
     ShortenedText = EncryptedText.split(":")[len(EncryptedText.split(":"))-2]
     RandomKey = ShortenedText[ShortenedText.index("/")+1:len(ShortenedText)]
     RandomToken = EncryptedText.split(":")[len(EncryptedText.split(":"))-1]
-    timestamp = datetime.utcfromtimestamp(Fernet(str.encode(RandomKey)).extract_timestamp(str.encode(RandomToken))).strftime('%d%H:%d%M:%d%S')
+    timestamp = datetime.fromtimestamp(Fernet(str.encode(RandomKey)).extract_timestamp(str.encode(RandomToken))).strftime('%d%H:%d%M:%d%S')
     Textkey = EncryptedText.split(":")[0]
     textToken = CleanToken(EncryptedText.split(":")[1], timestamp)
     RandomtextToken = CleanToken(EncryptedText.split(":")[2], timestamp)
